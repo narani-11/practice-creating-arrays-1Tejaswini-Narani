11362dae707dd02ee1406e3ac9441db781d7265d
narani-11 <naranitejaswini29@gmail.com>
2025-07-11 13:06:44 -0500

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,9 @@
+let groceryItems = ["Apples", "Bread", "Milk", "Eggs", "Rice"];
+let prices = [1.99, 2.49, 3.99];
+console.log(groceryItems);
+console.log(prices);
+console.log(groceryItems[0]);
+console.log(groceryItems[3]);
+console.log(groceryItems[4]);
+console.log(prices[0]);
+console.log(prices[2]);
